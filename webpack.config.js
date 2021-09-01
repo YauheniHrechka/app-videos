1aa194572e1789ee72b60981bac48ad9032b4b4a
Yauheni Hrechka <ip.grechko.ev@gmail.com>
2021-09-01 12:55:44 +0300

    Connected 'babel', 'react' and 'react-dom'. Added the 'App' component.

--- webpack.config.js
+++ webpack.config.js
@@ -4,13 +4,16 @@ const HTMLWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: "development",
-    entry: "./src/index.js",
+    entry: ["@babel/polyfill", "./src/index.jsx"],
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "[name].[hash].js"
     },
     devServer: {
         port: 3000
+    },
+    resolve: {
+        extensions: [".js", ".jsx"]
     },
     plugins: [
         new HTMLWebpackPlugin({ template: "./src/index.html" }),
@@ -21,6 +24,26 @@ module.exports = {
             {
                 test: /\.(css)$/,
                 use: ["style-loader", "css-loader"]
+            },
+            {
+                test: /\.m?js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-env"]
+                    }
+                }
+            },
+            {
+                test: /\.m?jsx$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader",
+                    options: {
+                        presets: ["@babel/preset-react", "@babel/preset-env"]
+                    }
+                }
             }
         ]
     }
